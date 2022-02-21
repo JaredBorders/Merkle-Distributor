0d763225f84c6f1e46776286db9fcf1bde175cb4
Jared Borders <jlbxsxs@gmail.com>
2022-02-20 22:27:57 -0600

    Remove unnecessary imports

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,5 @@
 import { task } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
-import 'hardhat-typechain';
-import '@nomiclabs/hardhat-truffle5';
-import 'hardhat-gas-reporter';
-import 'hardhat-contract-sizer';
-import '@openzeppelin/hardhat-upgrades';
-import 'solidity-coverage';
 
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
 	const accounts = await hre.ethers.getSigners();
@@ -15,14 +9,6 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
 	}
 });
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 export default {
-	solidity: '0.9.0',
-	networks: {
-		hardhat: {
-			allowUnlimitedContractSize: true,
-		},
-	},
+	solidity: '0.8.0',
 };
